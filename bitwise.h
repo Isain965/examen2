a28e61d99a40223fac8915c06155d95ff612aef7
Isain <isain.cuadra@konfio.mx>
2018-10-21 23:05:45 -0500

    Mas bonito

--- bitwise.h
+++ bitwise.h
@@ -3,33 +3,31 @@
 #include <string.h>
 
 struct  example  {
-        char type;
-        double val;
-        double dval;
-        int ival;
-        char *place;
-        };
+  char type;
+  double val;
+  double dval;
+  int ival;
+  char *place;
+};
 
 struct symbol {
-    char type;
-    char *name;
-  };
+  char type;
+  char *name;
+};
 
 struct quadruple {
-        char *op;
-        char *arg1;
-        char *arg2;
-        char *res;
-        };
+  char *op;
+  char *arg1;
+  char *arg2;
+  char *res;
+};
 
 #define NHASH 9997
   struct symbol symtab[NHASH];
-
   char find(char*);
 
 #define NQUAD 1000
   struct quadruple quadtab[NQUAD];
-
   struct symbol *lookup(char*);
 
 struct symbol *place;
